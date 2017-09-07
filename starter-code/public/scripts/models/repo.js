329ab67946426a136f1f3c244e2583d75a9e28a7
Raegan Millhollin <desdemonhu@gmail.com>
2017-09-06 19:04:31 -0700

    comments in repo.js, repoView.js, and newArticle.js

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,6 +6,9 @@ var app = app || {};
   repos.all = [];
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  ///Makes a request to server.js to pull information from GitHub, then puts those results in repos.all.
+  ///It is called in aboutController.js
+  ///It does call another function as the callback, which is a function in repoView.js
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -14,6 +14,9 @@ var app = app || {};
   const render = Handlebars.compile($('#repo-template').text());
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  ///Calls function ui, then applies the render template to every repo with the name property, and appends that to the ul element with an id of about
+  ///Called as the callback in aboutController
+  ///Yes, ui which is in repoView.js as well, and render (which is now a function), in repoView.js
   repoView.index = function() {
     ui();
 
